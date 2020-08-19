77cb726f1c2a94d8e12fbd37e10ae29868cb817c
johnnycuongn <cuongdn2001@gmail.com>
2020-08-19 19:33:45 +1000

    Drag Fix

--- Listing/Controller/List/+DropDelegate.swift
+++ Listing/Controller/List/+DropDelegate.swift
@@ -34,32 +34,6 @@ extension ListsViewController: UICollectionViewDropDelegate {
         if coordinator.proposal.operation == .move {
             self.reorderItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
         }
-        
-//        let destinationIndexPath: IndexPath
-//
-//        if let indexPath = coordinator.destinationIndexPath {
-//            destinationIndexPath = indexPath
-//        } else {
-//            // Get last index path of table view.
-//            let section = tableView.numberOfSections - 1
-//            let row = tableView.numberOfRows(inSection: section)
-//            destinationIndexPath = IndexPath(row: row, section: section)
-//        }
-//
-//        coordinator.session.loadObjects(ofClass: NSString.self) { items in
-//            // Consume drag items.
-//            let stringItems = items as! [String]
-//
-//            var indexPaths = [IndexPath]()
-//            for (index, item) in stringItems.enumerated() {
-//                let indexPath = IndexPath(row: destinationIndexPath.row + index, section: destinationIndexPath.section)
-//             let movingItem = Item(title: item)
-//             self.currentList.addItem(movingItem, from: .top)
-//                indexPaths.append(indexPath)
-//            }
-//
-//            tableView.insertRows(at: indexPaths, with: .automatic)
-//        }
     }
     
     fileprivate func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
